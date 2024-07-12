cf4fcd95dde97a5ceecc2e7fd84a7828bddf3ae9
sarthak965 <114921082+sarthak965@users.noreply.github.com>
2024-07-12 12:05:17 +0530

    Update linkedlistnode.java

--- linkedlistnode.java
+++ linkedlistnode.java
@@ -12,18 +12,7 @@ class Node {
 }
 
 public class linkedlistnode {
-    public static void main(String[] args) {
-        // Initialize an array
-        String[] array = {"one", "two", "three", "four"};
-
-        // Convert array to linked list
-        Node head = arrayToLinkedList(array);
-           Node temp=deleteel(head, "four");
-           printLinkedList(head);
-
-        // Print the linked list
-        
-    }
+  
 
     public static Node arrayToLinkedList(String[] array) {
         if (array.length == 0) {
@@ -108,4 +97,68 @@ public class linkedlistnode {
     }
     return head;
  }
+
+ public static Node inserthead(Node head, String val) {
+    Node temp = new Node (val, head);
+    return temp;
+  
+ }
+ public static Node inserttail(Node head, String val) {
+    Node temp = new Node (val, null);
+    Node temp2= head;
+    while (temp2.next!=null) {
+        temp2=temp2.next;
+        // did it own my own without video help, so happy :)
+    }
+    temp2.next=temp;
+    return head;
+  
+ }
+ public static Node insertkth(Node head, int k, String val) {
+    int count=0;
+    Node temp=head;
+   // Node temp3=head;
+    while (temp!=null) {
+        count++;
+        if( count== k-1) {
+            Node Newnode = new Node(val, temp.next);
+                 temp.next=Newnode;
+            
+            break;
+        }
+
+        temp=temp.next;
+    }
+    
+    return head;
+ }
+
+ public static Node entervaluebeforen(Node head, String val, String n) {
+           Node prev=null; 
+           Node temp=head;
+           while (temp!=null) {
+            if (temp.data==n) {
+                Node newnode= new Node (val,temp.next);
+                prev.next= newnode;
+                break;
+            }
+            prev=temp;
+            temp=temp.next;
+           }
+           
+           return head;
+ }
+ public static void main(String[] args) {
+    // Initialize an array
+    String[] array = {"one", "two", "three", "four"};
+
+    // Convert array to linked list
+    Node head = arrayToLinkedList(array);
+       Node temp= entervaluebeforen(head, "sarthak", "three");
+       printLinkedList(temp);
+
+    // Print the linked list
+    
+}
+
 }
